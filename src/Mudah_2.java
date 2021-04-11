27fc22c5dc17f52cbcfeb2309abcea66e131ebec
Yohanes <yohanesvelly23@gmail.com>
2021-04-11 17:04:21 +0700

    changed

--- src/Mudah_2.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-
-public class Mudah_2 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-    }
-}
