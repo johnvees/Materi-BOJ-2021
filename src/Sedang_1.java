d3cb099803cd21533e3498dd87610384f49d901d
Yohanes <yohanesvelly23@gmail.com>
2021-04-11 18:30:29 +0700

    fix update

--- src/Sedang_1.java
+++ src/Sedang_1.java
@@ -2,6 +2,31 @@ import java.io.*;
 
 public class Sedang_1 {
     public static void main(String[] args) throws IOException {
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("=== Program untuk membuat pola sisi terluar segi empat atau persegi ===");
+
+        int sisi;
+
+        System.out.print("Masukkan Panjang Sisi: ");
+        sisi = Integer.parseInt(br.readLine());
+
+        if (sisi > 2) {
+            for (int i = 0; i < sisi; i++) {
+                for (int j = 0; j < sisi; j++) {
+                    if ((i == 0) || (i == sisi - 1) || (j == 0) || (j == sisi - 1)) {
+                        System.out.print("*");
+                    } else {
+                        if (sisi % 2 == 1) {
+                            System.out.print(" ");
+                        } else {
+                            System.out.print(" ");
+                        }
+                    }
+                }
+                System.out.println("");
+            }
+        } else {
+            System.err.print("Sisi tidak lebih dari 2");
+        }
     }
 }
