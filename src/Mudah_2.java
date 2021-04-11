2f6091629538c8096eb7c3abee53817404cedd13
Yohanes <yohanesvelly23@gmail.com>
2021-04-11 15:41:58 +0700

    Enter

---
+++ src/Mudah_2.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Mudah_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
---
+++ src/Sedang_2.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class Sedang_2 {
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
